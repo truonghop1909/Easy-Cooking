a226239eabe1d127aa124064bab70b8775516ba9
unknown <truonghop1909@gmail.com>
2025-11-07 22:36:58 +0700

    them Ngan

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['i.pravatar.cc', 'picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc", // ảnh random avatar
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos", // ảnh random demo
+      },
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net", // Facebook avatar (nếu có)
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com", // Google avatar
+      },
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com", // GitHub avatar
+      },
+      {
+        protocol: "https",
+        hostname: "**.imgur.com", // Imgur (nếu dùng)
+      },
+    ],
   },
 };
 
